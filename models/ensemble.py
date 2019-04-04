0eff26370a48176dc2c751f8e76698bd9cc4ea67
Francisco Rivera <frtennis1@gmail.com>
2019-04-03 21:48:50 -0400

    Add exact ensemble

---
+++ models/ensemble.py
@@ -0,0 +1,14 @@
+from namedtensor import ntorch
+from namedtensor.nn import nn as nnn
+
+class ExactEnsemble(nnn.Module):
+    def __init__(self, models):
+        super().__init__()
+        self.models = nnn.ModuleList(models)
+        
+    def forward(self, hypothesis, premise):
+        log_preds = ntorch.stack([
+            model(hypothesis, premise) for model in self.models
+        ], 'model')
+        
+        return log_preds.softmax('classes').mean('model')
