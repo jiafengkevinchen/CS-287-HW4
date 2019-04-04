9b61568f1ec340b0a087b66d9656c613e58547cb
Yufeng Ling <yufengling@college.harvard.edu>
2019-04-04 15:01:46 -0400

    Fixed output dimension in decomp_attn.py

--- models/decomp_attn.py
+++ models/decomp_attn.py
@@ -8,8 +8,7 @@ class FeedFwd(nnn.Module):
                  dropout_p=.2, hidden_n=200):
         super().__init__()
         self.w1 = nnn.Linear(d_in, hidden_n).spec(name_in, "hidden")
-        self.w2 = nnn.Linear(hidden_n, hidden_n).spec("hidden", name_out)
-        # self.w3 = nnn.Linear(hidden_n, d_out).spec("hidden", name_out)
+        self.w2 = nnn.Linear(hidden_n, d_out).spec("hidden", name_out)
         self.drop = nnn.Dropout(p=dropout_p)
 
     def forward(self, x):
